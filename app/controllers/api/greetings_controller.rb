8dd9f1b36fa0253d07f11724d20e7aefc2356e6e
HASNAT KHAN <Hasnatking1947@gmail.com>
2023-08-15 20:21:17 +0500

    trying to fix it

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,8 +1,6 @@
-class Api::GreetingsController < ApplicationController
+class Api::V1::GreetingsController < ApplicationController
   def index
     @greeting = Greeting.order('RANDOM()').first
-
     render json: @greeting
   end
-end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,16 @@
-# frozen_string_literal: true
+require_relative "boot"
 
-require_relative 'boot'
-
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module MyNewRailsApp
+module HelloWorldRailsReact
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.assets.enabled = true
 
     # Configuration for the application, engines, and railties goes here.
     #
